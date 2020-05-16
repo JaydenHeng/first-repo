2caecd914e41db7d378c3ed2db01f331f9771a09
JaydenHeng <jayden_heng@hotmail.com>
2020-05-16 15:05:07 +0800

    first commit

---
+++ r_notes.r
@@ -0,0 +1,8 @@
+# installing the dslabs package
+install.packages("dslabs")
+# loading the dslabs package into the R session
+library(dslabs)
+
+install.packages("dslabs")  # to install a single package
+install.packages(c("tidyverse", "dslabs")） # to install two packages at the same time
+installed.packages() # to see the list of all installed packages
